c18e2d9ad8112befd1700f282378f4c1062aab90
Nicholsz20 <zachnichols1313@gmail.com>
2024-03-09 18:49:17 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+const KEY = '24ba6bf883a944a09e1f169a549f2c10'
 
 function App() {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${KEY}`);
+        setData(response.data);
+        setLoading(false)
+        
+      }catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    fetchData();
+    console.log(data)
+
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
