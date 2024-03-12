105171ea6e8087e0f26d10a44f11d13dfb595182
Nicholsz20 <zachnichols1313@gmail.com>
2024-03-12 13:31:19 -0400

    Added Recipe list to display contents from data

--- src/Components/RecipeList.tsx
+++ src/Components/RecipeList.tsx
@@ -1,7 +1,44 @@
-const RecipeList = () => {
+import { Recipe } from "../App";
+import RecipeInstructions from "./RecipeInstructions";
+
+
+interface RecipeListProps {
+    data: Recipe[]
+}
+
+const RecipeList = ({data}: RecipeListProps) => {
     return (
         <div>
-            RecipeList
+            {data.map((recipe) => <RecipeArray key={recipe.id} data={recipe}/>)}
+        </div>
+    )
+}
+
+interface RecipeArrayProps {
+    data: Recipe;
+}
+
+
+const RecipeArray = ({data}: RecipeArrayProps) => {
+    return(
+        <div key={data.id}>
+            <ul>
+                <li>
+                    {data.id}
+                </li>
+                    <h3>{data.title}</h3>
+                
+                <li>
+                    <img src={data.image} />
+                </li>
+                <li>
+                    <RecipeInstructions instructions={data.instructions} />
+                </li>
+                <li>
+                    <RecipeInstructions instructions={data.summary} />
+                </li>
+            </ul>
+
         </div>
     )
 }
