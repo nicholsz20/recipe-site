f1373260a49f226998281a2f3a26a20a0f10aac2
Nicholsz20 <zachnichols1313@gmail.com>
2024-03-13 15:36:38 -0400

    Updated RecipeList and refresh button with styles

--- src/Components/RecipeList.tsx
+++ src/Components/RecipeList.tsx
@@ -1,14 +1,25 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner, faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 
 import RecipeInstructions from "./RecipeInstructions";
 
 export interface Recipe {
   id: number;
   title: string;
-  instructions: string;
+  analyzedInstructions: [
+    {
+      steps: [
+        {
+          step: string;
+        }
+      ];
+    }
+  ];
+  dishTypes: string[];
+  diaryFree: boolean;
+  diets: string[];
   summary: string;
   image: string;
   cuisines: string[];
@@ -18,52 +29,60 @@ export interface SpoonacularResponse {
   recipes: Recipe[];
 }
 
-const KEY = "24ba6bf883a944a09e1f169a549f2c10";
+//If a key is full comment it out and uncomment a different one
+
+//const KEY = "24ba6bf883a944a09e1f169a549f2c10";
+const KEY = "cb61fb7dddc34daba2d7f61b391e90c1";
 
 const RecipeList = () => {
   const [data, setData] = useState<SpoonacularResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-    const fetchData = async () => {
-        setLoading(true)
-      try {
-        const response = await axios.get(
-          `https://api.spoonacular.com/recipes/random?apiKey=${KEY}`
-        );
-        setData(response.data);
-        
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const handleRefresh = () => {
-        fetchData();
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.spoonacular.com/recipes/random?apiKey=${KEY}`
+      );
+      console.log(response);
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleRefresh = () => {
+    fetchData();
+  };
 
   return (
     <div>
-    <button onClick={handleRefresh} disabled={loading}>Refresh Meal</button>
+      <button
+        className="refresh-btn bottom-right"
+        onClick={handleRefresh}
+        disabled={loading}
+      >
+        <FontAwesomeIcon
+          icon={loading ? faSpinner : faSyncAlt}
+          spin={loading}
+        />
+      </button>
       {loading ? (
         <div className="spinner-container">
           <FontAwesomeIcon icon={faSpinner} spin size="3x" />
           <p>Loading...</p>
         </div>
       ) : (
-        
         data?.recipes.map((recipe) => (
           <RecipeArray key={recipe.id} data={recipe} />
         ))
-        
-        
       )}
-      
     </div>
   );
 };
@@ -74,22 +93,23 @@ interface RecipeArrayProps {
 
 const RecipeArray = ({ data }: RecipeArrayProps) => {
   return (
-    <div key={data.id} className="text-center">
-      <ul>
-        <div>
-          <span hidden>{data.id}</span>
-          <h3>{data.title}</h3>
+    <div key={data.id} className="container">
+      <h3 className="title">{data.title}</h3>
+      <div className="content">
+        <img src={data.image} className="image" />
+        <div className="instructions">
+          <h2>Instructions</h2>
+          <p>
+            {data.analyzedInstructions[0].steps.map((step, index) => (
+              <li key={index}>{step.step}</li>
+            ))}
+          </p>
         </div>
-        <div>
-          <img src={data.image} />
-        </div>
-        <div>
-          <RecipeInstructions instructions={data.instructions} />
-        </div>
-        <div>
-          <RecipeInstructions instructions={data.summary} />
-        </div>
-      </ul>
+      </div>
+      <h4 className="title">Summary</h4>
+      <div className="summary">
+        <RecipeInstructions instructions={data.summary} />
+      </div>
     </div>
   );
 };
