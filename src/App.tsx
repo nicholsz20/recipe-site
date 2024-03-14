8567c5abc934d5181bc970bfe77810a35bc2f916
Nicholsz20 <zachnichols1313@gmail.com>
2024-03-14 14:38:07 -0400

    Updated Header and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,21 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import RecipeList from './Components/RecipeList';
+import Header from "./Components/Header";
+import { useState } from "react";
 
 
 function App() {
+  const [showNav, setShowNav] = useState(false);
+
+  const handleShowNav = () => {
+    setShowNav((prevShow) => !prevShow);
+  }
+
   return (
     <BrowserRouter>
-      <Navbar />
+    <Header handleShowNav={handleShowNav}/>
+      <Navbar showNav={showNav} />
         <Routes>
           <Route>
             <Route path="/" element={<Home />} />
