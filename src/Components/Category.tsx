37e6d105c1c48cef7017a2aa8102fad5cc5d3cdf
Nicholsz20 <zachnichols1313@gmail.com>
2024-03-15 10:24:42 -0400

    Added Images and styles to Category

--- src/Components/Category.tsx
+++ src/Components/Category.tsx
@@ -1,40 +1,83 @@
 const mealType = [
-    { cat: "main course", img: "" },
-    { cat: "side dish", img: "" },
-    { cat: "dessert", img: "" },
-    { cat: "appetizer", img: "" },
-    { cat: "salad", img: "" },
-    { cat: "bread", img: "" },
-    { cat: "breakfest", img: "" },
-    { cat: "soup", img: "" },
-    { cat: "beverage", img: "" },
-    { cat: "sauce", img: "" },
-    { cat: "marinade", img: "" },
-    { cat: "fingerfood", img: "" },
-    { cat: "snack", img: "" },
-    { cat: "drink", img: "" },
-  ];
-  
-  const Category = () => {
-    return <div>{mealType.map((meal, index) => <CatType key={index} meal={meal} />)}</div>;
-  };
-  
-  interface MealType {
-    cat: string;
-    img: string;
-  }
-  
-  interface MealTypes {
-    meal: MealType;
-  }
-  
-  const CatType = ({ meal }: MealTypes) => {
-    return (
-      <div>
-        <h5>{meal.cat}</h5>
+  {
+    cat: "main course",
+    img: "https://spoonacular.com/recipeImages/637335-556x370.jpg",
+  },
+  {
+    cat: "side dish",
+    img: "https://spoonacular.com/recipeImages/635846-556x370.jpg",
+  },
+  {
+    cat: "dessert",
+    img: "https://spoonacular.com/recipeImages/579247-556x370.jpg",
+  },
+  {
+    cat: "appetizer",
+    img: "https://spoonacular.com/recipeImages/663338-556x370.jpg",
+  },
+  {
+    cat: "salad",
+    img: "https://spoonacular.com/recipeImages/796684-556x370.jpg",
+  },
+  {
+    cat: "bread",
+    img: "https://spoonacular.com/recipeImages/636133-312x231.jpg",
+  },
+  {
+    cat: "breakfest",
+    img: "https://spoonacular.com/recipeImages/665307-312x231.jpg",
+  },
+  {
+    cat: "soup",
+    img: "https://spoonacular.com/recipeImages/653687-556x370.jpg",
+  },
+  {
+    cat: "beverage",
+    img: "https://spoonacular.com/recipeImages/1096295-312x231.jpg",
+  },
+  {
+    cat: "sauce",
+    img: "https://spoonacular.com/recipeImages/633068-556x370.jpg",
+  },
+  {
+    cat: "snack",
+    img: "https://spoonacular.com/recipeImages/651437-556x370.jpg",
+  },
+];
+
+const Category = () => {
+  return (
+    <>
+    <h1 className="cat">Categories</h1>
+    <div className="cat-container">   
+      {mealType.map((meal, index) => (
+        <CatType key={index} meal={meal} />
+      ))}
+      
+    </div>
+    <span className="cat">View All</span>
+    </>
+  );
+};
+
+interface MealType {
+  cat: string;
+  img: string;
+}
+
+interface MealTypes {
+  meal: MealType;
+}
+
+const CatType = ({ meal }: MealTypes) => {
+  return (
+    
+      <div className="cat">
+        <img src={meal.img} alt={meal.cat} className="cat-img" />
+        <h3 className="cat-title">{meal.cat}</h3>
       </div>
-    );
-  };
-  
-  export default Category;
-  
+    
+  );
+};
+
+export default Category;
