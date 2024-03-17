ded94114fc40c5b78825ca9db0a8d0b35ed9bf7e
Nicholsz20 <zachnichols1313@gmail.com>
2024-03-17 17:50:59 -0400

    Fixed Bug for api call on recipes and added main and app notes

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
   const handleShowNav = () => {
     setShowNav((prevShow) => !prevShow);
   }
+  //s
 
   return (
     <BrowserRouter>
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -11,7 +11,7 @@ import './Home.css';
 
 
 const Home = () => {
-    // State for search input
+    // State for search input, s
     const [searchInput, setSearchInput] = useState('');
     
 
