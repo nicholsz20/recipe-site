9471c3a13e5d9b0c0f91288e443430ee4cd488d5
Nicholsz20 <zachnichols1313@gmail.com>
2024-03-18 17:32:18 -0400

    Basic Similar Recipes Added, No styles yet

--- src/Components/SearchPage.tsx
+++ src/Components/SearchPage.tsx
@@ -9,17 +9,17 @@ import Pagination from './Pageination';
 //const KEY = "24ba6bf883a944a09e1f169a549f2c10";
 //const KEY = "cb61fb7dddc34daba2d7f61b391e90c1";
 //const KEY = "e3de5d36255e46babdbd21cbbbf5ec38";
-//const KEY = "fa0f376a27884351b9f852d1ae5e20f8";
-const KEY = "50980bc1ff884ed68509e87da2cf5db1";
+const KEY = "fa0f376a27884351b9f852d1ae5e20f8";
+//const KEY = "50980bc1ff884ed68509e87da2cf5db1";
 
-interface SearchResponse {
+export interface SearchResponse {
   results: Recipe[];
   offset: number;
   number: number;
   totalResults: number;
 }
 
-interface Recipe {
+export interface Recipe {
   id: string;
   title: string;
   image: string;
