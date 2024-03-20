a7d43cff420560ee72d5f4157e3b5592300a9db6
fhassan2 <fhassan2@ncsu.edu>
2024-03-20 15:52:10 -0400

    added global api file

--- src/Components/RecipeList.tsx
+++ src/Components/RecipeList.tsx
@@ -5,6 +5,7 @@ import { faSpinner, faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 
 import RecipeInstructions from "./RecipeInstructions";
 import BackButton from "./BackButton";
+import KEY from './apiConfig'; 
 
 export interface Recipe {
   id: string;
@@ -30,12 +31,7 @@ export interface SpoonacularResponse {
   recipes: Recipe[];
 }
 
-//If a key is full comment it out and uncomment a different one
-const KEY = "fa0f376a27884351b9f852d1ae5e20f8"
 
-//const KEY = "24ba6bf883a944a09e1f169a549f2c10";
-//const KEY = "cb61fb7dddc34daba2d7f61b391e90c1";
-//const KEY = "e3de5d36255e46babdbd21cbbbf5ec38";
 
 const RecipeList = () => {
   const [data, setData] = useState<SpoonacularResponse | null>(null);
--- src/Components/SearchPage.tsx
+++ src/Components/SearchPage.tsx
@@ -7,12 +7,8 @@ import BackButton from './BackButton';
 import Pagination from './Pageination';
 import SearchBar from "./SearchBar";
 import './SearchPage.css';
+import KEY from './apiConfig'; 
 
-// const KEY = "24ba6bf883a944a09e1f169a549f2c10";
-// const KEY = "cb61fb7dddc34daba2d7f61b391e90c1";
-// const KEY = "e3de5d36255e46babdbd21cbbbf5ec38";
-// const KEY = "fa0f376a27884351b9f852d1ae5e20f8";
-const KEY = "50980bc1ff884ed68509e87da2cf5db1";
 
 export interface SearchResponse {
   results: Recipe[];
